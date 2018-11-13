1c7d862d275ac327588a343b19b6a3afb28b5378
Radovan Hajdukovic <radovan.hajdukovic@dreamlines.com>
2018-11-13 02:56:04 +0100

    test by moving whole boilerplate code to styled-components

--- packages/react-scripts/template-typescript/src/index.tsx
+++ packages/react-scripts/template-typescript/src/index.tsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import styled from 'styled-components';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Logo = styled.img`
+  animation: ${rotate} infinite 20s linear;
+  height: 40vmin;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
+
 
 const Header = styled.header`
   background-color: #282c34;
@@ -17,22 +40,21 @@ const Header = styled.header`
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper>
         <Header>
-          <img src={logo} className="App-logo" alt="logo" />
+          <Logo src={logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <Link
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </Link>
         </Header>
-      </div>
+      </Wrapper>
     );
   }
 }
